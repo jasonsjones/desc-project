e6584e20f3329a2f2472eb145239ec3dea7baae5
Jason S. Jones <jsjones96@gmail.com>
2019-08-09 16:55:20 -0700

    feat: add useEffect hook to get auth'd user

--- packages/app/src/containers/Layout.js
+++ packages/app/src/containers/Layout.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Nav from '../components/Nav/Nav';
 import { AuthProvider } from '../context/AuthContext';
 
 const Layout = props => {
     const [contextUser, setContextUser] = useState(null);
     const [token, setToken] = useState('');
+    useEffect(() => {
+        fetch('http://localhost:3000/api/users/me', {
+            method: 'GET',
+            credentials: 'include'
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+            })
+            .then(data => {
+                if (data) {
+                    setContextUser(data.payload.user);
+                    setToken(data.payload.token);
+                }
+            });
+    }, []);
 
     const login = (user, token) => {
         setContextUser(user);
