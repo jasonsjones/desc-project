b56e8361db6de7d52e43c4a75ffcc6fba3dd5b2f
Jason S. Jones <jsjones96@gmail.com>
2019-08-09 16:54:28 -0700

    feat: add AuthContext to Home cmp

--- packages/app/src/components/Home.js
+++ packages/app/src/components/Home.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../context/AuthContext';
 
 const Home = () => {
-    return <h3 className="center-align teal-text">Home Page</h3>;
+    const authCtx = useContext(AuthContext);
+    return (
+        <>
+            {authCtx.contextUser ? (
+                <h3 className="center-align teal-text">
+                    Welcome, {`${authCtx.contextUser.name.first}`}
+                </h3>
+            ) : (
+                <h3 className="center-align teal-text">Home Page</h3>
+            )}
+        </>
+    );
 };
 
 export default Home;
