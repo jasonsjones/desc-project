9b80f2b7f5360b55346fd598081f4eccf2187ec2
Jason S. Jones <jsjones96@gmail.com>
2019-08-10 08:37:39 -0700

    refactor: abstract nav user dropdown to component

--- packages/app/src/components/Nav/Nav.js
+++ packages/app/src/components/Nav/Nav.js
@@ -2,8 +2,9 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import SignedOutLinks from './SignedOutLinks';
 import SignedInLinks from './SignedInLinks';
+import UserDropdown from './UserDropdown';
+import AuthContext from '../../context/AuthContext';
 import './Nav.css';
-import M from 'materialize-css';
 
 import AuthContext from '../../context/AuthContext';
 
@@ -40,35 +41,10 @@ const Nav = () => {
                 </Link>
                 <ul className="right hide-on-med-and-down">
                     {!isAuthed && <SignedOutLinks />}
-                    {isAuthed && <SignedInLinks />}
-                    {isAuthed && initDropdown() && (
+                    {isAuthed && (
                         <>
-                            <li className="profile-menu">
-                                <a href="#!" className="dropdown-trigger" data-target="profile">
-                                    <i
-                                        style={{ marginLeft: '10px' }}
-                                        className="small material-icons prefix"
-                                    >
-                                        account_circle
-                                    </i>
-                                    <span style={{ margin: '0 8px' }}>
-                                        {`${authCtx.contextUser.name.first} ${authCtx.contextUser.name.last}`}
-                                    </span>
-                                    <i className={`small material-icons prefix`}>expand_more</i>
-                                </a>
-                            </li>
-                            <div>
-                                <ul id="profile" className="dropdown-content">
-                                    <li>
-                                        <a href="#!">Profile</a>
-                                    </li>
-                                    <li>
-                                        <Link to="/" onClick={handleLogout}>
-                                            Logout
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
+                            <SignedInLinks />
+                            <UserDropdown user={authCtx.contextUser} handleLogout={handleLogout} />
                         </>
                     )}
                 </ul>
---
+++ packages/app/src/components/Nav/UserDropdown.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import M from 'materialize-css';
+
+const UserDropdwon = ({ user, handleLogout }) => {
+    useEffect(() => {
+        const elems = document.querySelectorAll('.dropdown-trigger');
+        M.Dropdown.init(elems, {
+            coverTrigger: false
+        });
+    }, []);
+
+    return (
+        <>
+            <li className="profile-menu">
+                <a href="#!" className="dropdown-trigger" data-target="profile">
+                    <i style={{ marginLeft: '10px' }} className="small material-icons prefix">
+                        account_circle
+                    </i>
+                    <span style={{ margin: '0 8px' }}>
+                        {`${user.name.first} ${user.name.last}`}
+                    </span>
+                    <i className={`small material-icons prefix`}>expand_more</i>
+                </a>
+            </li>
+            <div>
+                <ul id="profile" className="dropdown-content">
+                    <li>
+                        <a href="#!">Profile</a>
+                    </li>
+                    <li>
+                        <Link to="/" onClick={handleLogout}>
+                            Logout
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
+};
+
+export default UserDropdwon;
