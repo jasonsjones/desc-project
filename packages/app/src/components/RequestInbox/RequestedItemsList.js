da3f0e9184038b45445a97e8dc0969fa344c4eb3
eabehr <emilyabehrendt@gmail.com>
2019-10-03 16:41:00 -0700

    fix - get the collapsible rows to expand on first click without a refresh

--- packages/app/src/components/RequestInbox/RequestedItemsList.js
+++ packages/app/src/components/RequestInbox/RequestedItemsList.js
@@ -5,20 +5,28 @@ import RequestRow from './RequestRow';
 
 class RequestedItemsList extends React.Component {
     componentDidMount() {
-        document.addEventListener('DOMContentLoaded', function() {
-            const elems = document.querySelectorAll('.collapsible.expandable');
-            M.Collapsible.init(elems, { accordion: false });
-        });
+        const elems = document.querySelectorAll('.collapsible.expandable');
+        M.Collapsible.init(elems, { accordion: false });
     }
 
     render() {
         return (
-            <ul className="collapsible expandable">
-                {this.props.items &&
-                    this.props.items
-                        .filter(item => item.status === this.props.type)
-                        .map(item => <RequestRow key={item._id} {...item} />)}
-            </ul>
+            <div>
+                <ul className="collapsible expandable with-header">
+                    <li class="collection-header">
+                        <div className="row">
+                            <div className="col offset-s1 ">Requestor</div>
+                            <div className="col offset-s1 ">Item</div>
+                            <div className="col offset-s2 ">Quantity</div>
+                            <div className="col offset-s3">Request Date</div>
+                        </div>
+                    </li>
+                    {this.props.items &&
+                        this.props.items
+                            .filter(item => item.status === this.props.type)
+                            .map(item => <RequestRow key={item._id} {...item} />)}
+                </ul>
+            </div>
         );
     }
 }
