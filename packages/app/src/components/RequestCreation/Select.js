5ee5e2c49c8d921ce730fe9a3e1fdb5c09408ccf
eabehr <emilyabehrendt@gmail.com>
2020-01-21 16:21:49 -0800

    I hate materialize... https://stackoverflow.com/questions/36339354/how-do-i-get-the-materialize-select-dropdown-to-work-with-react

--- packages/app/src/components/RequestCreation/Select.js
+++ packages/app/src/components/RequestCreation/Select.js
@@ -4,7 +4,12 @@ const Select = props => {
     return (
         <div className="form-group">
             <label htmlFor={props.name}> {props.title} </label>
-            <select name={props.name} value={props.value} onChange={props.handleChange}>
+            <select
+                className="browser-default"
+                name={props.name}
+                value={props.value}
+                onChange={props.handleChange}
+            >
                 <option value="" disabled>
                     {props.placeholder}
                 </option>
