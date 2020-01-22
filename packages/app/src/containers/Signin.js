961f06dce93a348167866dc9624e77ad905acc6f
Jason S. Jones <jsjones96@gmail.com>
2020-01-22 07:05:58 -0800

    fix: redirect to /signup /signin to home if logged in

--- packages/app/src/containers/Signin.js
+++ packages/app/src/containers/Signin.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import SigninForm from '../components/SigninForm/SigninForm';
+import AuthContext from '../context/AuthContext';
 
 const Signin = props => {
+    const authCtx = useContext(AuthContext);
+    if (authCtx.contextUser) {
+        return <Redirect to="/" />;
+    }
     return <SigninForm {...props} />;
 };
 
--- packages/app/src/containers/Signup.js
+++ packages/app/src/containers/Signup.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import SignupForm from '../components/SignupForm/SignupForm';
+import AuthContext from '../context/AuthContext';
 
 const Signup = props => {
+    const authCtx = useContext(AuthContext);
+    if (authCtx.contextUser) {
+        return <Redirect to="/" />;
+    }
     return <SignupForm {...props} />;
 };
 
