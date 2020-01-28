e59f6246f2189b165d22ce1470af47dc05690a0c
Jason S. Jones <jsjones96@gmail.com>
2020-01-27 18:58:44 -0800

    test: restructure signup tests

--- packages/e2e/cypress/integration/signup.spec.js
+++ packages/e2e/cypress/integration/signup.spec.js
@@ -15,156 +15,154 @@ describe('User Signup', () => {
         cy.exec('npm run db:reset');
     });
 
-    describe('Signup', () => {
-        it('does not submit form if data is missing', () => {
-            cy.visit('http://localhost:4200/signup');
-            cy.get('#firstName')
-                .focus()
-                .type(userData.firstName);
+    it('does not submit form if data is missing', () => {
+        cy.visit('http://localhost:4200/signup');
+        cy.get('#firstName')
+            .focus()
+            .type(userData.firstName);
 
-            // skip last name
+        // skip last name
 
-            cy.get('#email')
-                .focus()
-                .type(userData.email);
+        cy.get('#email')
+            .focus()
+            .type(userData.email);
 
-            cy.get('input.select-dropdown')
-                .focus()
-                .click();
-            cy.get('ul.dropdown-content')
-                .contains(userData.program)
-                .click();
+        cy.get('input.select-dropdown')
+            .focus()
+            .click();
+        cy.get('ul.dropdown-content')
+            .contains(userData.program)
+            .click();
 
-            cy.get('#password')
-                .focus()
-                .type(userData.password);
-            cy.get('#confirmPassword')
-                .focus()
-                .type(userData.password);
+        cy.get('#password')
+            .focus()
+            .type(userData.password);
+        cy.get('#confirmPassword')
+            .focus()
+            .type(userData.password);
 
-            cy.get('button')
-                .contains('Sign Up')
-                .click();
+        cy.get('button')
+            .contains('Sign Up')
+            .click();
 
-            cy.url().should('eq', 'http://localhost:4200/signup');
-        });
+        cy.url().should('eq', 'http://localhost:4200/signup');
+    });
 
-        it('displays a message when the passwords do not match', () => {
-            cy.visit('http://localhost:4200/signup');
-            cy.get('#firstName')
-                .focus()
-                .type(userData.firstName);
-            cy.get('#lastName')
-                .focus()
-                .type(userData.lastName);
-            cy.get('#email')
-                .focus()
-                .type(userData.email);
+    it('displays a message when the passwords do not match', () => {
+        cy.visit('http://localhost:4200/signup');
+        cy.get('#firstName')
+            .focus()
+            .type(userData.firstName);
+        cy.get('#lastName')
+            .focus()
+            .type(userData.lastName);
+        cy.get('#email')
+            .focus()
+            .type(userData.email);
 
-            cy.get('input.select-dropdown')
-                .focus()
-                .click();
-            cy.get('ul.dropdown-content')
-                .contains(userData.program)
-                .click();
+        cy.get('input.select-dropdown')
+            .focus()
+            .click();
+        cy.get('ul.dropdown-content')
+            .contains(userData.program)
+            .click();
 
-            cy.get('#password')
-                .focus()
-                .type(userData.password);
-            cy.get('#confirmPassword')
-                .focus()
-                .type('doesnotmatch');
+        cy.get('#password')
+            .focus()
+            .type(userData.password);
+        cy.get('#confirmPassword')
+            .focus()
+            .type('doesnotmatch');
 
-            cy.get('.helper-text').should('contain', 'Passwords do NOT match');
-        });
+        cy.get('.helper-text').should('contain', 'Passwords do NOT match');
+    });
 
-        it('does not submit form if the passwords do not match', () => {
-            cy.visit('http://localhost:4200/signup');
-            cy.get('#firstName')
-                .focus()
-                .type(userData.firstName);
-            cy.get('#lastName')
-                .focus()
-                .type(userData.lastName);
-            cy.get('#email')
-                .focus()
-                .type(userData.email);
+    it('does not submit form if the passwords do not match', () => {
+        cy.visit('http://localhost:4200/signup');
+        cy.get('#firstName')
+            .focus()
+            .type(userData.firstName);
+        cy.get('#lastName')
+            .focus()
+            .type(userData.lastName);
+        cy.get('#email')
+            .focus()
+            .type(userData.email);
 
-            cy.get('input.select-dropdown')
-                .focus()
-                .click();
-            cy.get('ul.dropdown-content')
-                .contains(userData.program)
-                .click();
+        cy.get('input.select-dropdown')
+            .focus()
+            .click();
+        cy.get('ul.dropdown-content')
+            .contains(userData.program)
+            .click();
 
-            cy.get('#password')
-                .focus()
-                .type(userData.password);
-            cy.get('#confirmPassword')
-                .focus()
-                .type('doesnotmatch');
+        cy.get('#password')
+            .focus()
+            .type(userData.password);
+        cy.get('#confirmPassword')
+            .focus()
+            .type('doesnotmatch');
 
-            cy.get('button')
-                .contains('Sign Up')
-                .click();
+        cy.get('button')
+            .contains('Sign Up')
+            .click();
 
-            cy.url().should('eq', 'http://localhost:4200/signup');
-        });
+        cy.url().should('eq', 'http://localhost:4200/signup');
+    });
 
-        it('signs up a new user', () => {
-            cy.visit('http://localhost:4200/signup');
-            cy.get('#firstName')
-                .focus()
-                .type(userData.firstName);
-            cy.get('#lastName')
-                .focus()
-                .type(userData.lastName);
-            cy.get('#email')
-                .focus()
-                .type(userData.email);
+    it('signs up a new user', () => {
+        cy.visit('http://localhost:4200/signup');
+        cy.get('#firstName')
+            .focus()
+            .type(userData.firstName);
+        cy.get('#lastName')
+            .focus()
+            .type(userData.lastName);
+        cy.get('#email')
+            .focus()
+            .type(userData.email);
 
-            cy.get('input.select-dropdown')
-                .focus()
-                .click();
-            cy.get('ul.dropdown-content')
-                .contains(userData.program)
-                .click();
+        cy.get('input.select-dropdown')
+            .focus()
+            .click();
+        cy.get('ul.dropdown-content')
+            .contains(userData.program)
+            .click();
 
-            cy.get('#password')
-                .focus()
-                .type(userData.password);
-            cy.get('#confirmPassword')
-                .focus()
-                .type(userData.password);
+        cy.get('#password')
+            .focus()
+            .type(userData.password);
+        cy.get('#confirmPassword')
+            .focus()
+            .type(userData.password);
 
-            cy.get('button')
-                .contains('Sign Up')
-                .click();
+        cy.get('button')
+            .contains('Sign Up')
+            .click();
 
-            cy.url().should('eq', 'http://localhost:4200/');
+        cy.url().should('eq', 'http://localhost:4200/');
 
-            cy.get('nav .profile-menu a span').should(
-                'contain',
-                `${userData.firstName} ${userData.lastName}`
-            );
-            cy.get('h3').should('contain', userData.firstName);
-        });
+        cy.get('nav .profile-menu a span').should(
+            'contain',
+            `${userData.firstName} ${userData.lastName}`
+        );
+        cy.get('h3').should('contain', userData.firstName);
+    });
 
-        it('maintains new user context on page reload', () => {
-            cy.reload();
-            cy.get('h3').should('contain', userData.firstName);
-        });
+    it('maintains new user context on page reload', () => {
+        cy.reload();
+        cy.get('h3').should('contain', userData.firstName);
+    });
 
-        it('logs out the user after signup', () => {
-            cy.visit('http://localhost:4200/');
+    it('logs out the user after signup', () => {
+        cy.visit('http://localhost:4200/');
 
-            cy.get('.profile-menu > a').click();
-            cy.get('.profile-menu a')
-                .contains('Logout')
-                .click();
-            cy.url().should('eq', 'http://localhost:4200/');
-            cy.get('h3').should('contain', 'Home Page');
-            cy.get('h3').not('contain', userData.firstName);
-        });
+        cy.get('.profile-menu > a').click();
+        cy.get('.profile-menu a')
+            .contains('Logout')
+            .click();
+        cy.url().should('eq', 'http://localhost:4200/');
+        cy.get('h3').should('contain', 'Home Page');
+        cy.get('h3').not('contain', userData.firstName);
     });
 });
