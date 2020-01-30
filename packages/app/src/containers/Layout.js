ad2c311cef956471cb458404b443f3a74483abcc
Jason S. Jones <jsjones96@gmail.com>
2020-01-29 21:07:39 -0800

    fix: add check for data.payload

--- packages/app/src/containers/Layout.js
+++ packages/app/src/containers/Layout.js
@@ -10,7 +10,7 @@ const Layout = props => {
 
     useEffect(() => {
         fetchSessionUser().then(data => {
-            if (data) {
+            if (data && data.payload) {
                 setContextUser(data.payload.user);
                 setToken(data.payload.token);
             }
