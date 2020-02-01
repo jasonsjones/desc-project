44bd4c88c3a67ba3d311d31bd5f15bfdf8ba7ac6
Jason S. Jones <jsjones96@gmail.com>
2020-01-31 21:54:16 -0800

    style: adjust margin between notes

--- packages/app/src/components/RequestorInbox/index.js
+++ packages/app/src/components/RequestorInbox/index.js
@@ -58,7 +58,7 @@ const initCollapsibleElements = () => {
 const NoteDetails = ({ note }) => {
     const { first, last } = note.submittedBy.name;
     return (
-        <p>
+        <p style={{ margin: '.125rem 0' }}>
             <span style={{ fontWeight: 'bold' }}>{`${first} ${last}: `}</span> {note.body}
         </p>
     );
