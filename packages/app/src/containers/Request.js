4449d9097a5d909f700eb77c59fbc89a3f98ffd2
Jason S. Jones <jsjones96@gmail.com>
2020-02-15 13:42:01 -0800

    fix: change name of form component

--- packages/app/src/containers/Request.js
+++ packages/app/src/containers/Request.js
@@ -2,7 +2,7 @@ import React from 'react';
 import RequestForm from '../components/RequestForm/';
 
 const Request = props => {
-    return <NewRequestForm {...props} />;
+    return <RequestForm {...props} />;
 };
 
 export default Request;
