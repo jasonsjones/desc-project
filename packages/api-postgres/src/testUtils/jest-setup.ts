4602f7d9a96aee56d7f4ad9fa9b4f02656f4c816
Jason S. Jones <jsjones96@gmail.com>
2020-02-27 16:52:56 -0800

    chore: move jest config to file and add test setup

---
+++ packages/api-postgres/src/testUtils/jest-setup.ts
@@ -0,0 +1,13 @@
+/*
+  Might be a good idea to have different setup files for different types of tests.
+  These set up files can be referrenced by specific jest.config.json files depending
+  on the test type (e.g. unit, integration, or acceptance)
+*/
+
+beforeAll(() => {
+    console.log('This will run before each test file, but may not have access to postres db yet');
+});
+
+afterAll(() => {
+    console.log('This will run after each test file, but the postgres may be already closed...');
+});
