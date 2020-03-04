f03e59ecc1b9f513c7e7497b5519d77fc3abe77f
Jason S. Jones <jsjones96@gmail.com>
2020-03-03 19:53:34 -0800

    fix: update user service

--- packages/api-postgres/src/user/UserService.ts
+++ packages/api-postgres/src/user/UserService.ts
@@ -36,10 +36,7 @@ export default class UserService {
 
     static async deleteUser(id: string): Promise<User | undefined> {
         const user = UserService.getUserById(id);
-        const deleteOp = await User.delete({ id });
-        if (deleteOp.affected === 1) {
-            return user;
-        }
-        return Promise.resolve(undefined);
+        await User.delete({ id });
+        return user;
     }
 }
