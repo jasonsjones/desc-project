8584bd856c9a8bb866058b5e39e5206f787bebf6
Jason S. Jones <jsjones96@gmail.com>
2020-03-22 13:04:54 -0700

    fix: clean up item entity

--- packages/api-postgres/src/entity/Item.ts
+++ packages/api-postgres/src/entity/Item.ts
@@ -1,21 +1,8 @@
-import {
-    Column,
-    Entity,
-    PrimaryGeneratedColumn,
-    // TableInheritance,
-    BaseEntity,
-    ManyToOne,
-    JoinColumn
-} from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, BaseEntity, ManyToOne, JoinColumn } from 'typeorm';
 import User from './User';
-// import { HouseholdItems } from '../item/types';
-// import { EngagementItems } from '../item/types';
 
 @Entity()
-// @TableInheritance({ column: { type: 'varchar', name: 'category' } })
 export default class Item extends BaseEntity {
-    // protected abstract name: EngagementItems | HouseholdItems;
-
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
@@ -31,7 +18,6 @@ export default class Item extends BaseEntity {
 
     // clientId: string;
     // clientRequest -> ClientRequest
-    // submittedBy -> User
     // location: string;
     // numberOfItems: number;
     // urgency: string;
