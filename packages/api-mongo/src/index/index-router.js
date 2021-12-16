a1c54ef5264c244baf2397bfa21ef2f505ff4571
Jason S. Jones <jsjones96@gmail.com>
2021-12-16 15:27:01 -0800

    chore: remove unused api-mongo package

--- packages/api-mongo/src/index/index-router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as IndexController from './index-controller';
-
-export default () => {
-    let indexRouter = express.Router();
-    indexRouter.get('/api', IndexController.getRootAPIRoute);
-    indexRouter.get('/', IndexController.renderIndexPage);
-    return indexRouter;
-};
