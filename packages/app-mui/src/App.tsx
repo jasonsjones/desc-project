4e71e5634aa03102f771034e553d0f1fc2818667
Jason S. Jones <jsjones96@gmail.com>
2022-01-01 14:55:05 -0800

    chore: [MUI] add skeleton MUI app package

---
+++ packages/app-mui/src/App.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+
+function App() {
+    return (
+        <div className="App">
+            <Typography variant="h1" component="h1">
+                Welcome to DESC Portal!
+            </Typography>
+        </div>
+    );
+}
+
+export default App;
